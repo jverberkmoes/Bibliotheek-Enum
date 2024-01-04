87a26aa0e012e0e51713a997e51d108da45aa44d
Jos Verberkmoes <jos.verberkmoes@distrian.com>
2024-01-04 11:46:11 +0100

    Add files via upload

---
+++ Data/BibliotheekContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bibliotheek.Datamodels;
+
+namespace Bibliotheek.Data
+{
+    public class BibliotheekContext : DbContext
+    {
+        public BibliotheekContext (DbContextOptions<BibliotheekContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bibliotheek.Datamodels.Boek> Boek { get; set; } = default!;
+    }
+}
