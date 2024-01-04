8f15c95d626f7f5cc6a57c6d34bc405222a3c5c7
Jos Verberkmoes <jos.verberkmoes@distrian.com>
2024-01-04 11:47:12 +0100

    Add files via upload

---
+++ Datamodels/Boek.cs
@@ -0,0 +1,29 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bibliotheek.Datamodels
+{
+    public class Boek
+    {
+        [Key]
+        public int Boek_ID { get; set; }
+        [Display(Name = "Genre")]
+        public Boekgenres Boek_Genre { get; set; }
+        [Display(Name = "Titel")]
+        public string? Boek_Titel { get; set; }
+        [Display(Name = "Serienummer")]
+        public int Boek_Serienummer { get; set; }
+        [Display(Name = "Schrijver")]
+        public string? Boek_Schrijver { get; set; }
+         [Display(Name = "Prijs")]
+        public decimal Boek_Prijs { get; set; }
+        [Display(Name = "ISBN")]
+        public string? Boek_ISBN { get; set; }
+        [Display(Name = "Uitgifte")]
+        public DateTime Boek_Uitgifte { get; set; }
+        [Display(Name = "Gelezen")]
+        public DateTime Boek_Gelezen { get; set; }
+    }
+}
---
+++ Datamodels/Boekgenres.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Drawing;
+using System;
+using System.Xml.Linq;
+
+namespace Bibliotheek.Datamodels;
+
+public enum Boekgenres
+{
+    [Display(Name = "None")] None = 0,
+    [Display(Name = "Roman")] Roman = 1,
+    [Display(Name = "Thriller")] Thriller = 2,
+    [Display(Name = "ScienceFiction")] ScienceFiction = 3,
+    [Display(Name = "Biografie")] Biografie = 4
+}
